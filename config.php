68ee7a7c3f0085bf610101db0b505ff4cf74fad6
Unknown <sandu.rajapakse@lankanmon.com>
2018-01-25 20:56:21 -0500

    Created Config File
    Also added .gitignore to exclude local test config file

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+// DB Hostname
+define('DB_HOST','localhost');
+// DB Name
+define('DB_NAME','database name');
+// DB Access Username
+define('DB_USER','username');
+// DB Access Password
+define('DB_PSWD','password');
+
