9e1e26c42b160253521415c5f435906d1f7fd278
Unknown <sandu.rajapakse@lankanmon.com>
2018-01-26 01:41:43 -0500

    Added nessasary files for logging

--- config.php
+++ config.php
@@ -7,4 +7,9 @@ define('DB_NAME','database name');
 define('DB_USER','username');
 // DB Access Password
 define('DB_PSWD','password');
-
+// Log Usage in Text File? (1 = Yes / 0 = No)
+$logUsage = 1;
+$logUsageFile = "logs.txt";
+// Log Errors in Text File? (1 = Yes / 0 = No)
+$logErrors = 1;
+$logErrorsFile = "errors.txt";
